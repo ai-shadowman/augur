f96c5143e93cc5ae694ddb35d5b9709623535a1c
Tola Awofolu <agapebondservant@yahoo.com>
2026-08-10 15:22:11 -0400

    Updated Git URL description

--- workflows/examples/code_understanding/utils/prompt_utils.py
+++ workflows/examples/code_understanding/utils/prompt_utils.py
@@ -16,18 +16,18 @@ def prepare_indexing_config(graphrag_source_path: str, git_slug: str, git_repo: 
     loader.download("graphrag/summarize_descriptions.txt", download_dir="prompts")
     loader.download("graphrag/global_search_reduce_system_prompt.txt", download_dir="prompts")
 
-    substitutions = {
-        "{git_slug}": git_slug,
-        "{git_repo}": git_repo,
-        "{multi_repo}": str(multi_repo).lower(),
-    }
-
-    for prompt_file in os.listdir("prompts"):
-        prompt_path = os.path.join("prompts", prompt_file)
-        with open(prompt_path) as f:
-            content = f.read()
-        with open(prompt_path, "w") as f:
-            f.write(reduce(lambda updated, pair: updated.replace(*pair), substitutions.items(), content))
+    # substitutions = {
+    #     "{git_slug}": git_slug,
+    #     "{git_repo}": git_repo,
+    #     "{multi_repo}": str(multi_repo).lower(),
+    # }
+    #
+    # for prompt_file in os.listdir("prompts"):
+    #     prompt_path = os.path.join("prompts", prompt_file)
+    #     with open(prompt_path) as f:
+    #         content = f.read()
+    #     with open(prompt_path, "w") as f:
+    #         f.write(reduce(lambda updated, pair: updated.replace(*pair), substitutions.items(), content))
 
     shutil.copytree("prompts", f"{graphrag_source_path}/prompts", dirs_exist_ok=True)
 
