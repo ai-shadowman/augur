d494cf7fdd9b1004dec60863fc69b9e3fa75a2ca
Tola Awofolu <agapebondservant@yahoo.com>
2026-08-11 15:39:06 -0400

    Testing with MaaS

--- workflows/examples/code_understanding/pipelines/base/data_generation.py
+++ workflows/examples/code_understanding/pipelines/base/data_generation.py
@@ -159,6 +159,7 @@ def get_parsed_code_metadata(df, language, config=False):
             max_tokens=32_000,
             response_format={"type": "json_object"},
             top_k=1,
+            n=1
         )
 
         converted_dataset = flow.generate(dataset, max_concurrency=10)
